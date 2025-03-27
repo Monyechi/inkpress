26684adcc98c2bc09864ac6f5ae938f148f6d797
Monyechi <63468925+Monyechi@users.noreply.github.com>
2025-03-27 16:39:43 +0100

    register Post model in admin and add PostDetailView for detailed post display

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Post
 
-# Create your views here.
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'articles/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
+        context['content'] = self.object.content
+        return context
